61f08c1c391c04d9896348560fdc517827fe9694
Zac Cooper <zacooper5g@outlook.com>
2022-11-04 09:51:02 +1100

    created fetch request for backend data to be displaye

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,34 @@
+import { useEffect, useState } from "react"
+
 export const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    const [workouts, setWorkouts] = useState(null)
+
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const response = await fetch('/api/workouts')
+            const json = await response.json()
+            
+            if (response.ok) {
+                setWorkouts(json)
+            }
+        };
+
+        fetchWorkouts()
+    }, [])
+
+    console.log(workouts)
+
+    return (
+        <div className="home" >
+            <div className="workouts" >
+                {workouts && workouts.map((workout) => (
+                    <div key={workout._id}>
+                        <h2>Title: {workout.title}</h2>
+                        <h2>Load: {workout.load}</h2>
+                    </div>
+                ))}
+            </div>
+        </div>
+
+    )
 }
