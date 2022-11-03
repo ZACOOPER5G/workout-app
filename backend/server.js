24bf94ef0cdfe7a1e2ba4787d6268d00329e928c
Zac Cooper <zacooper5g@outlook.com>
2022-11-03 11:55:33 +1100

    connected to mongoose database

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,30 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts')
 
 // express app
 const app = express();
 
 // middleware
+app.use(express.json());
+
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
 });
 
 // routes
-app.use('/api/workouts', workoutRoutes)
+app.use('/api/workouts', workoutRoutes);
 
-//listening
-app.listen(process.env.PORT, () => {
-    console.log("Listening on port: " + process.env.PORT);
-});
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        //listening
+        app.listen(process.env.PORT, () => {
+            console.log("Connected to db and listening on port: " + process.env.PORT);
+        });
+    })
+    .catch(err => {
+        console.log(err)
+    });
