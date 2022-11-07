1998c44760c3e8742919238bdcd5b4508ea4d2e9
Zac Cooper <zacooper5g@outlook.com>
2022-11-07 13:29:20 +1100

    updated display of workouts

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,11 +1,11 @@
 
 export const WorkoutDetails = ( props ) => {
   return (
-    <div>
-        <h2>Title: <em>{ props.workout.title }</em></h2> 
-        <p><strong>Load:</strong> <em>{ props.workout.load }</em></p>
+    <div className="workout-details" >
+        <h4>{ props.workout.title }</h4> 
+        <p><strong>Load:</strong> <em>{ props.workout.load }KG</em></p>
         <p><strong>Reps:</strong> <em>{ props.workout.reps }</em></p>
-        <h6>Posted at { props.workout.createdAt }</h6>
+        <p>Posted at { props.workout.createdAt }</p>
     </div>
   )
 };
