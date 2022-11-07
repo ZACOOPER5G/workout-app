0df724f039365a2e249a8a9ea31d1d1039ac6faa
Zac Cooper <zacooper5g@outlook.com>
2022-11-07 15:25:00 +1100

    added dynamic classes for empty form fields

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -1,11 +1,13 @@
 import { useState } from "react"
-
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 
 export const WorkoutForm = () => {
+    const { dispatch } = useWorkoutsContext();
     const [title, setTitle] = useState('');
     const [load, setLoad] = useState('');
     const [reps, setReps] = useState('');
     const [error, setError] = useState(null);
+    const [emptyFields, setEmptyFields] = useState([]);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -21,12 +23,18 @@ export const WorkoutForm = () => {
 
         if (!response.ok) {
             setError(json.error);
+            setEmptyFields(json.emptyFields)
         }
         if (response.ok) {
             setTitle('');
             setLoad('');
             setReps('');
             setError(null);
+            setEmptyFields([]);
+            dispatch({
+                type: 'CREATE_WORKOUT',
+                payload: json
+            })
             console.log('New workout added!', json)
         }
     }
@@ -39,18 +47,21 @@ export const WorkoutForm = () => {
                 type="text"
                 onChange={(e) => setTitle(e.target.value)}
                 value={title}
+                className={emptyFields.includes('title') ? "error" : ""}
             />
             <label>Load (KG)</label>
             <input 
                 type="number"
                 onChange={(e) => setLoad(e.target.value)}
                 value={load}
+                className={emptyFields.includes('load') ? "error" : ""}
             />
             <label>Reps</label>
             <input 
                 type="number"
                 onChange={(e) => setReps(e.target.value)}
                 value={reps}
+                className={emptyFields.includes('reps') ? "error" : ""}
             />
             <button >Add workout</button>
             { error && <div className="error">{ error }</div> }
