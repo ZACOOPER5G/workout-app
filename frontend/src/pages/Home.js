2b147fc5b20008aca34d6bdea8483082aa50674c
Zac Cooper <zacooper5g@outlook.com>
2022-11-07 13:15:03 +1100

    updated local state to be global from workout context hook

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,12 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 // components
 import { WorkoutDetails } from "../components/WorkoutDetails";
 import { WorkoutForm } from "../components/WorkoutForm";
+import { useWorkoutContext } from "../hooks/useWorkoutsContext";
 
 export const Home = () => {
-    const [workouts, setWorkouts] = useState(null)
+    const { workouts, dispatch } = useWorkoutContext();
 
     useEffect(() => {
         const fetchWorkouts = async () => {
@@ -13,14 +14,15 @@ export const Home = () => {
             const json = await response.json()
             
             if (response.ok) {
-                setWorkouts(json)
+                dispatch({
+                    type: 'SET_WORKOUTS',
+                    payload: json
+                })
             }
         };
 
         fetchWorkouts()
-    }, [])
-
-    console.log(workouts)
+    }, []);
 
     return (
         <div className="home" >
