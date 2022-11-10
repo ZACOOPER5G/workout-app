c7a6a4e83eba2b9db2a405d4c64c9f9a6f779911
Zac Cooper <zacooper5g@outlook.com>
2022-11-11 10:25:49 +1100

    added user routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,14 @@
 // login user
-
+const loginUser = async (req, res) => {
+    res.json({msg: "User has logged in"})
+};
 
 // signup user
+const signupUser = async (req, res) => {
+    res.json({msg: "Signup user"})
+}
 
-
+module.exports = {
+    loginUser,
+    signupUser
+};
