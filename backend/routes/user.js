f6285f31a62e3aba82e9b9c23337b8e847138b96
Zac Cooper <zacooper5g@outlook.com>
2022-11-11 10:09:44 +1100

    added placeholder post requests

---
+++ backend/routes/user.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const router = express.Router();
+
+// login route
+router.post("/login", async (req, res) => {
+
+});
+
+// signup route
+router.post("/signup", async (req, res) => {
+    
+});
+
+module.exports = router;
