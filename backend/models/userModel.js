b902e3ade3a128e481955e86c74c8eb2fbf6b51c
Zac Cooper <zacooper5g@outlook.com>
2022-11-11 11:06:46 +1100

    added static signup method

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require("bcrypt");
 
 const Schema = mongoose.Schema;
 
@@ -13,5 +14,24 @@ const userSchema = new Schema({
         required: true
     }
 });
+
+// static signup method
+userSchema.statics.signup = async (email, password) => {
+    const exists = await this.findOne({ email: email })
+
+    if (exists) {
+        throw Error("Email already exists")
+    };
+
+    let salt = await bcrypt.genSalt(10);
+    let hash = await bcrypt.hash(password, salt);
+
+    const user = await this.create({
+        email,
+        password: hash
+    });
+
+    return user;
+};
 
 module.exports = mongoose.model('User', userSchema);
