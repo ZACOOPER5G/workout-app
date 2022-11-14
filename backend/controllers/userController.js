7b86eacf75635ede7b49aaf86ed0bd80552fd658
Zac Cooper <zacooper5g@outlook.com>
2022-11-14 15:31:55 +1100

    added login logic

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const createToken = (_id) => {
     let token = jwt.sign({ _id }, process.env.SECRET, { expiresIn: '24h' });
@@ -9,7 +10,16 @@ const createToken = (_id) => {
 
 // login user
 const loginUser = async (req, res) => {
-    res.json({msg: "Login user"})
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.login(email, password);
+        // create token 
+        let token = createToken(user._id);
+        res.status(200).json({ user, token })
+    } catch (err) {
+        res.status(400).json({ error: err.message })
+    }
 };
 
 // signup user
