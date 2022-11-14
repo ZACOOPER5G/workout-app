518084b49fafbc404dd33bda716c988c0abfca07
Zac Cooper <zacooper5g@outlook.com>
2022-11-14 14:59:50 +1100

    added web token for user sign up

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,31 @@
+require('dotenv').config();
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+    let token = jwt.sign({ _id }, process.env.SECRET, { expiresIn: '24h' });
+    return token;
+};
+
 // login user
 const loginUser = async (req, res) => {
-    res.json({msg: "User has logged in"})
+    res.json({msg: "Login user"})
 };
 
 // signup user
 const signupUser = async (req, res) => {
-    res.json({msg: "Signup user"})
-}
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.signup(email, password);
+
+        // create token 
+        let token = createToken(user._id);
+        res.status(201).json({email, token})
+    } catch (err) {
+        res.status(400).json({ error: err.message })
+    };
+};
 
 module.exports = {
     loginUser,
