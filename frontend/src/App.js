d7aa1fb944e6270cda2b2ac5a1d816c02b92921f
Zac Cooper <zacooper5g@outlook.com>
2022-11-15 15:49:25 +1100

    added login and signup routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import { Navbar } from './components/Navbar';
+
+// pages and components
+import { Home } from './pages/Home';
+import { Login } from './pages/Login';
+import { Signup } from './pages/Signup';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <div className='pages'>
           <Routes>
-            <Route path="/" element={""} />
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
           </Routes>
         </div>
       </BrowserRouter>
