9b457da031854f04c7b71df547ef9c33ab653adb
Zac Cooper <zacooper5g@outlook.com>
2022-11-16 12:48:02 +1100

    frontend now keeps user logged in upon refresh

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,19 +1,17 @@
-import { createContext, useReducer } from 'react';
+import { createContext, useReducer, useEffect } from 'react';
 
 export const AuthContext = createContext();
 
 export const authReducer = (state, action) => {
     switch (action.type) {
-        case "LOGIN": {
+        case "LOGIN": 
             return {
                 user: action.payload
             }
-        }
-        case "LOGOUT": {
+        case "LOGOUT":
             return {
                 user: null
             }
-        }
         default: return state
     }
 };
@@ -23,7 +21,17 @@ export const AuthContextProvider = ({ children }) => {
         user: null
     });
 
-    console.log("AuthContext state: " + state)
+    useEffect(() => {
+        const user = JSON.parse(localStorage.getItem("user"));
+        if (user) {
+            dispatch({
+                type: "LOGIN",
+                payload: user
+            });
+        };
+    }, []);
+
+    console.log("AuthContext state: ", state);
 
     return (
         <AuthContext.Provider value={{ ...state, dispatch }} >
