5318b596021de64844d119f020bef35e2840c0c6
Zac Cooper <zacooper5g@outlook.com>
2022-11-17 10:10:21 +1100

    added auth protection middleware

---
+++ backend/middleware/requireAuth.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
+
+const requireAuth = (req, res, next) => {
+    // verify user authentication
+
+    let { authorization } = req.headers;
+
+    if (!authorization) {
+        return res.status(401).json({error: "Authorization token required"});
+    };
+
+    let token = authorization.split(' ')[1];
+
+    try {
+        const _id = jwt.verify(token, process.env.SECRET);
+        req.user = await User.findById({ _id }).select("_id");
+        next();
+    } catch (err) {
+        console.log(err);
+        res.status(401).json({error: "Request is not authorized"});
+    }
+};
+
+module.exports = requireAuth;
